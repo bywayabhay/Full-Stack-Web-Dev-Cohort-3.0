a4ba9c00687cc33c9b91bacfdd9d1e04954501eb
Abhay Singh <singhabhay2085@gmail.com>
2026-09-05 22:42:29 +0530

    variables, dataTypes, conversion

---
+++ 01-Frontend/03-JavaScript/01-JavaScript-Day-01-Phase-1/Learning/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 734894
+// accountId = 78 // -> not allowed
+
+let accountEmail = "abhay@gmail.com"
+var accountPassword = "292Ahbhay"
+let accountState
+
+accountCity = "Jaunpur"
+
+/*
+prefer not to use in var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountCity, accountState]);
---
+++ 01-Frontend/03-JavaScript/01-JavaScript-Day-01-Phase-1/Learning/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // treat all JS code as newer version
+// account = 89 //-> now I have define it 
+// console.log(account);
+
+
+// alert(9+ 9) // we're using nodejs, not browser
+
+//number -> 2^53
+//bigInt
+//string -> ""
+//boolean -> true / false
+//null -> standalone value
+//undefined
+//symbol -> unique
